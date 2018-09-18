c5783677d9bf1c758f91ceb37d25a1ae518b6c9c
SEUNGMIN KIM <smk950226@naver.com>
2018-09-18 20:49:05 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <ActivityIndicator />
+        <Text>WOW!! Wonderful!!!아아아아
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
