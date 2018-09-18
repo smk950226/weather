7151b599d6414599e8b0e515aa419afafbb9d52b
SEUNGMIN KIM <smk950226@naver.com>
2018-09-18 21:05:16 +0900

    style 적용

--- App.js
+++ App.js
@@ -5,9 +5,12 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <ActivityIndicator />
-        <Text>WOW!! Wonderful!!!아아아아
-        </Text>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}>
+          <View style={styles.redViewSmall}/>
+          <View style={styles.blueViewSmall}/>
+        </View>
+        <View style={styles.blueView}/>
       </View>
     );
   }
@@ -17,7 +20,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  redView: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  yellowView: {
+    flex: 7,
+    backgroundColor: 'yellow',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+  redViewSmall: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+  },
+  blueViewSmall: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'blue',
+  }
 });
