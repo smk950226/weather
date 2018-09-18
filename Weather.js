576b86ea4bbfadcf9b881606d44875c63779f6db
SEUNGMIN KIM <smk950226@naver.com>
2018-09-18 22:32:23 +0900

    상태바 숨김 및 아이콘 불러오기

--- Weather.js
+++ Weather.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Weather extends Component {
     render(){
         return (
         <LinearGradient colors={['#00c6fb', '#005bea']} style={styles.container}>
             <View style={styles.upper}>
-                <Text>Icon here</Text>
+                <Ionicons color="white" size={144} name='ios-rainy' />
                 <Text style={styles.temp}>35</Text>
             </View>
             <View style={styles.lower}>
                 <Text style={styles.title}>Rainny</Text>
-                <Text style={styles.subtitle}>More</Text>
+                <Text style={styles.subtitle}>우산챙기세요!</Text>
             </View>
         </LinearGradient>
         )
@@ -27,9 +28,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: 'transparent',
     },
     temp: {
-        fontSize: 38,
+        fontSize: 48,
         backgroundColor: 'transparent',
         color: 'white',
         marginTop: 10,
