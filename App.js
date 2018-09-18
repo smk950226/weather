bdc331fd2125062b159c517a6619436f839becb4
SEUNGMIN KIM <smk950226@naver.com>
2018-09-18 23:37:59 +0900

    위치정보 사용 에러처리

--- App.js
+++ App.js
@@ -5,27 +5,31 @@ import Weather from './Weather'
 export default class App extends Component {
   state = {
     isLoaded: false,
+    error: null,
   };
-  componentDidMount(){
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({
-          isLoaded: true
+          isLoaded: true,
         });
       },
       error => {
-        console.log(error);
+        this.setState({
+          error: error
+        });
       }
     );
   }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
       <StatusBar hidden={true} />
         {isLoaded ? <Weather /> : (
           <View style={styles.loading}>
-            <Text style={styles.loadingText}>Getting Wheather..</Text>
+            <Text style={styles.loadingText}>Getting Wheather..!</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -37,6 +41,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  errorText: {
+    color: 'red',
+    backgroundColor: 'transparent',
+    marginBottom: 40
   },
   loading: {
     flex: 1,
